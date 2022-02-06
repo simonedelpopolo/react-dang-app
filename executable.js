31e312c834011d729589ed9b50d8ebddd6e2aa4a
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-02-06 11:16:38 +0000

    removed serve and flag libraries ⬇︎
    now the project use Koorie as experimental server
    https://github.com/simonedelpopolo/koorie

--- executable.js
+++
@@ -1,9 +0,0 @@
-import { flags, server } from './index.js'
-
-// Splicing out from `process.argv` the paths for node and executable.js
-process.argv.splice( 0, 2 )
-
-// Process name.
-process.title = 'r-dang-serve'
-
-await server( await flags( process.argv ) )
